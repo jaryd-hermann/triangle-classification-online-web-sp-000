a7ab198520c32018615cce24fb510c60b109f6d1
Jaryd Hermann <hermannjaryd@gmail.com>
2021-03-13 20:13:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,8 +1,10 @@
 class Triangle
 attr_accessor :equilateral, :isosceles, :scalene
 
-def initialize(triangle_sides)
-  triangle_sides.each {|key, value| self.send(("#{key}="), value)}
+def initialize(equilateral:,isosceles:, scalene:)
+  @equilateral = equilateral
+  @isosceles = isosceles
+  @scalene = scalene
 end
 
 
